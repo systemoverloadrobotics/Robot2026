2ef1b37957263ab99238bb0276b6185a8cdc77b7
jeannebrigitjudy <jeanne.brigit.judy@gmail.com>
2026-01-18 17:04:24 -0800

    I wrote the constants file adding new variables to the Intake class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,18 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+  
+    public static class Intake {
+      public static final double kP = 0;
+      public static final double kI = 0;
+      public static final double kD = 0;
+      public static final int motorId1 = 0; //change this number 0 is placeholder
+      public static final int pivotId = 0; //need to change number
+      public static final int CANcoderId = 0; //need to change number
+      public static final int PivotPID = 0; //need to change number
+      public static final int PivotSensorToMechanism = 0;//need to change number
+      public static final int PivotCANcoderOffset = 0;//need to change number
+      public static final int StartPower = 0;//need to change number
+    }
+
 }
