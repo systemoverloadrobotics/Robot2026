dfe49ebb91f052ccf5ff587b21e5092efb062b19
sahana-hi <sahana.venkatr@gmail.com>
2026-01-18 16:24:44 -0800

    IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,44 +4,122 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//importing stuff for encoders
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue; //figure out why its red and fix it
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+//Dutcycle - how fast motor spins
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//to use constants file
+import frc.robot.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  //create intake motor
+  private final TalonFX rollerMotor; //creates motor on roller
+
+  private final TalonFX pivotIntakeMotor; //creates motor on pivot
+
+  private final DutyCycleOut dutyCycleReq = new DutyCycleOut(0); //regulates motor speed for roller, skeptical about pivot
+
+  private final CANcoder pivotCANcoder; //pivot encoder
+
+  private final CANBus canBus; //Creates canbus
+
+  private final PositionVoltage pivotPosReq; //Creates Position Voltage request
+
+  /** Creates a new Intake. */
+  public IntakeSubsystem() {
+    rollerMotor = new TalonFX(Constants.Intake.motorId1);
+
+    canBus = new CANBus("rio");
+
+    pivotIntakeMotor = new TalonFX(Constants.Intake.pivotId, canBus); 
+
+    pivotCANcoder = new CANcoder(Constants.Intake.CANcoderId, canBus);
+
+    pivotPosReq = new PositionVoltage(0);
+
+    Slot0Configs slot0Configs = new Slot0Configs();
+    slot0Configs.kP = 0; //random number not 0
+    slot0Configs.kI = 0; //random number not 0
+    slot0Configs.kD = 0; //random number not 0
+
+    var MOCPivot = new MotorOutputConfigs();
+    MOCPivot.Inverted = InvertedValue.CounterClockwise_Positive;
+    MOCPivot.NeutralMode = NeutralModeValue.Brake;
+
+    var feedbackConfigs = new FeedbackConfigs(); //should not be red fix it
+    feedbackConfigs.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor; //should not be red fix it
+    feedbackConfigs.SensorToMechanismRatio = 1.0; //need to change number not permanent
+
+    var currentLimitsConfigs = new CurrentLimitsConfigs();
+    currentLimitsConfigs.SupplyCurrentLimitEnable = true;
+    currentLimitsConfigs.SupplyCurrentLimit = 30; //random number fix it
+
+    var pivotConfig = new TalonFXConfiguration();
+    pivotConfig.Slot0 = slot0Configs;
+    pivotConfig.MotorOutput = MOCPivot;
+    pivotConfig.Feedback = feedbackConfigs;
+    pivotConfig.CurrentLimits = currentLimitsConfigs;
+    pivotIntakeMotor.getConfigurator().apply(pivotConfig);
+
+    CANcoderConfiguration pivotCANcoderConfig = new CANcoderConfiguration(); //Creates encoder configuration
+    pivotCANcoderConfig.MagnetSensor = new MagnetSensorConfigs()
+      .withMagnetOffset(0.0);
+    pivotCANcoder.getConfigurator().apply(pivotCANcoderConfig);
+    //Absolute encoder position --> internal encoder for pivot
+    pivotIntakeMotor.setPosition(pivotCANcoder.getAbsolutePosition().getValueAsDouble());
+ 
+  }
+
+  public void setPivotPosition(double position) { //should not be red fix it
+      pivotIntakeMotor.setControl(pivotPosReq.withPosition(position));
+    }
+
+    public void setPivotPosition(Angle position) {
+      //uses angle measurement
+      pivotIntakeMotor.setControl(pivotPosReq.withPosition(position));
+    }
 
   /**
    * Example command factory method.
    *
    * @return a command
    */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+
+  public void start() {
+    this.setPower(0); //change this number placeholder 0
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public void stop() {
+    this.setPower(0); //change this number placeholder 0
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void setPower(double power) {
+    //write comments about rotations 1 rotation: 360 degrees
+    rollerMotor.setControl(dutyCycleReq.withOutput(power));
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+  
 }
