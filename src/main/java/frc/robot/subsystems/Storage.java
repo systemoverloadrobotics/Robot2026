8bdb8539b1758cf79e5c507adc7fe700ae594f99
Pranav Pulavarthi <pran.edtech@gmail.com>
2026-01-20 22:49:25 -0800

    Removed state and commands from the storage

--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -9,9 +9,8 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -21,7 +20,8 @@ import frc.robot.Constants;
  * 
  */
 public class Storage extends SubsystemBase {
-    private RollerState rollerState = RollerState.OFF;
+    private TalonFX rollerMotor;
+    private DutyCycleOut rollerOutput = new DutyCycleOut(0);
 
     public Storage() {
         configureMotors();
@@ -31,59 +31,37 @@ public class Storage extends SubsystemBase {
     private void configureMotors() {
         rollerMotor = new TalonFX(Constants.Storage.rollerMotorId);
 
-        var motorOutput = new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake);
+        var MOCRoller = new MotorOutputConfigs()
+            .withNeutralMode(NeutralModeValue.Brake)
+            .withInverted(InvertedValue.CounterClockwise_Positive);
         TalonFXConfiguration motorConfig = new TalonFXConfiguration()
                 .withCurrentLimits(new CurrentLimitsConfigs()
                         .withSupplyCurrentLimit(35)
                         .withSupplyCurrentLimitEnable(true))
-                .withMotorOutput(motorOutput);
+                .withMotorOutput(MOCRoller);
 
         rollerMotor.getConfigurator().apply(motorConfig);
     }
 
     public enum RollerState {
-        FORWARD, //Forward to feed Shooter
-        REVERSE, //Reverse to pass fuel
-        OFF // rollers turned off
-    }
-
-    private TalonFX rollerMotor;
-
-    public Command rollersForward() {
-        return runOnce(
-                () -> {
-                    this.rollerState = RollerState.FORWARD;
-                });
-    }
-
-    public Command rollersOff() {
-        return runOnce(
-                () -> {
-                    this.rollerState = RollerState.OFF;
-                });
-    }
-
-    public Command rollersReverse() {
-        return runOnce(
-                () -> {
-                    this.rollerState = RollerState.REVERSE;
-                });
-    }
-
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-        DutyCycleOut rollerOutput = new DutyCycleOut(0);
-        switch (rollerState) {
-            case FORWARD -> rollerOutput.Output = Constants.Storage.ROLLER_FORWARD_SPEED;
-            case REVERSE -> rollerOutput.Output = Constants.Storage.ROLLER_REVERSE_SPEED;
-            case OFF -> rollerOutput.Output = 0;
+        FORWARD(Constants.Storage.ROLLER_FORWARD_SPEED), //Forward to feed Shooter
+        REVERSE(Constants.Storage.ROLLER_REVERSE_SPEED), //Reverse to pass fuel
+        OFF(0); // rollers turned off
+        
+        private double rollerSpeed;
+        
+        private RollerState(double rollerSpeed) {
+            this.rollerSpeed = rollerSpeed;
         }
-        rollerMotor.setControl(rollerOutput);
+
+        public String getDescription() {
+            return "" + this.rollerSpeed;
+        }
+        
     }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    public void setRollers(RollerState rollerState) {
+        rollerOutput = rollerOutput.withOutput(rollerState.rollerSpeed);
+        rollerMotor.setControl(rollerOutput);
     }
 }
