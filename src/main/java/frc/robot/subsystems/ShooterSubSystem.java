eb79308b2d27940b21de0dff42f10ffc9b293566
Adil-Gadi <58380369+Adil-Gadi@users.noreply.github.com>
2026-02-03 19:11:38 -0800

    Standardizes shooter subsystem measurements

--- src/main/java/frc/robot/subsystems/ShooterSubSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubSystem.java
@@ -9,8 +9,14 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.FeetPerSecond;
 import static frc.robot.Constants.Shooter.*;
 
 public class ShooterSubSystem extends SubsystemBase {
@@ -31,8 +37,8 @@ public class ShooterSubSystem extends SubsystemBase {
     private PositionVoltage hoodAngleRequest = new PositionVoltage(0);
 
     // ========== SETTERS ==========
-    private double targetFlywheelFPS = 0.0;
-    private double targetHoodAngleDegrees = 0.0;
+    private LinearVelocity targetFlywheel = FeetPerSecond.of(0.0);
+    private Angle targetHoodAngleDegrees = Degrees.of(0.0);
 
     public ShooterSubSystem() {
         configureSubSystem();
@@ -45,20 +51,20 @@ public class ShooterSubSystem extends SubsystemBase {
         flywheelMotor2 = new TalonFX(FLYWHEEL_SECOND_MOTOR_ID);
         hoodAngleMotor = new TalonFX(HOOD_ANGLE_MOTOR_ID);
 
-        //configure flywheel motor
+        // configure flywheel motor
         TalonFXConfiguration flywheelConfig = new TalonFXConfiguration();
-        flywheelConfig.Slot0.kP =FLYWHEEL_kP;
+        flywheelConfig.Slot0.kP = FLYWHEEL_kP;
         flywheelConfig.Slot0.kI = FLYWHEEL_kI;
         flywheelConfig.Slot0.kD = FLYWHEEL_kD;
         flywheelConfig.Slot0.kV = FLYWHEEL_kV;
         flywheelMotor1.getConfigurator().apply(flywheelConfig);
         flywheelMotor2.getConfigurator().apply(flywheelConfig);
 
-        //configure hoodAnglemotor
+        // configure hoodAnglemotor
         TalonFXConfiguration hoodConfig = new TalonFXConfiguration();
         hoodConfig.Slot0.kP = HOOD_ANGLE_KP;
-        hoodConfig.Slot0.kI = HOOD_ANGLE_KI;  
-        hoodConfig.Slot0.kD = HOOD_ANGLE_KD; 
+        hoodConfig.Slot0.kI = HOOD_ANGLE_KI;
+        hoodConfig.Slot0.kD = HOOD_ANGLE_KD;
         // Neutral mode - brake to hold position
         hoodConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         // Configured with FusedCANcoder feedback
@@ -74,49 +80,46 @@ public class ShooterSubSystem extends SubsystemBase {
         hoodCANcoder.getConfigurator().apply(cancoderConfig);
     }
 
-    public void setFlywheelVelocity(double feetPerSecond) {
-        this.targetFlywheelFPS = feetPerSecond;
+    public void setFlywheelVelocity(LinearVelocity velocity) {
+        this.targetFlywheel = velocity;
         // ft/s to RPS: ft/s ÷ circumference_ft × gear_ratio
-        double rps = (feetPerSecond / FLYWHEEL_CIRCUMFERENCE_FT) * FLYWHEEL_GEAR_RATIO;
+        double rps = (velocity.in(FeetPerSecond) / FLYWHEEL_CIRCUMFERENCE_FT) * FLYWHEEL_GEAR_RATIO;
         flywheelMotor1.setControl(flywheelVelocityRequest.withVelocity(rps));
         flywheelMotor2.setControl(flywheelVelocityRequest.withVelocity(rps));
     }
 
-    public void setHoodAngle(double degrees) {
-        this.targetHoodAngleDegrees = degrees;
+    public void setHoodAngle(Angle angle) {
+        this.targetHoodAngleDegrees = angle;
         // Convert degrees to motor rotations
-        double rotations = degrees * HOOD_ANGLE_GEAR_RATIO / 360.0;
+        double rotations = angle.in(Degrees) * HOOD_ANGLE_GEAR_RATIO / 360.0;
         hoodAngleMotor.setControl(hoodAngleRequest.withPosition(rotations));
     }
 
-    public double getFlywheelVelocity() {
+    public LinearVelocity getFlywheelVelocity() {
         double rps = flywheelMotor1.getVelocity().getValueAsDouble();
-        return (rps / FLYWHEEL_GEAR_RATIO) * FLYWHEEL_CIRCUMFERENCE_FT;
+        // return (rps / FLYWHEEL_GEAR_RATIO) * FLYWHEEL_CIRCUMFERENCE_FT;
+        return FeetPerSecond.of(rps / FLYWHEEL_GEAR_RATIO * FLYWHEEL_CIRCUMFERENCE_FT);
     }
 
-    public double getHoodAngle() {
-        double rotations = hoodAngleMotor.getPosition().getValueAsDouble();
-        return (rotations * 360.0) / HOOD_ANGLE_GEAR_RATIO;
+    public Angle getHoodAngle() {
+        hoodAngleMotor.getPosition().refresh();
+        return hoodAngleMotor.getPosition().getValue();
     }
 
     public boolean isFlywheelAtTarget() {
-        double error = Math.abs(getFlywheelVelocity() - targetFlywheelFPS);
+        double error = Math.abs(getFlywheelVelocity().in(FeetPerSecond) - targetFlywheel.in(FeetPerSecond));
         return error <= FLYWHEEL_FPS_TOLERANCE;
     }
 
     public boolean isHoodAngleAtTarget() {
-        double error = Math.abs(getHoodAngle() - targetHoodAngleDegrees);
+        double error = Math.abs(getHoodAngle().in(Degrees) - targetHoodAngleDegrees.in(Degrees));
         return error <= HOOD_ANGLE_TOLERANCE;
     }
+
     public boolean isAtTarget() {
         return isFlywheelAtTarget() && isHoodAngleAtTarget();
     }
-    public void stop() {
-        targetFlywheelFPS = 0;
-        flywheelMotor1.stopMotor();
-        flywheelMotor2.stopMotor();
-        hoodAngleMotor.stopMotor();
-    }
+
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
