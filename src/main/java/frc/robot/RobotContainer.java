5fc40f650fd029d8f08b765471c0062c8f0f46ec
Adil-Gadi <58380369+Adil-Gadi@users.noreply.github.com>
2026-02-03 20:19:56 -0800

    Fixes issue with shoot process

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,11 +13,15 @@ import frc.robot.subsystems.Storage;
 import frc.robot.subsystems.Storage.RollerState;
 
 import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.FeetPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import javax.sound.sampled.Line;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -25,9 +29,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -37,31 +44,38 @@ public class RobotContainer {
   private final Storage m_storage = new Storage();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-    
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
@@ -83,22 +97,25 @@ public class RobotContainer {
     var launchAngle = getLaunchAngle(distance);
 
     return Commands.run(() -> {
-          m_shooterSubsystem.setHoodAngle(launchAngle.in(Degree));
-          m_shooterSubsystem.setFlywheelVelocity(flywheelSpeed.in(RotationsPerSecond) * 60.0);
-        }, m_shooterSubsystem)
+      m_shooterSubsystem.setHoodAngle(launchAngle);
+      m_shooterSubsystem.setFlywheelVelocity(flywheelSpeed);
+    }, m_shooterSubsystem)
         .andThen(Commands.waitUntil(
             () -> m_shooterSubsystem.isFlywheelAtTarget() && m_shooterSubsystem.isHoodAngleAtTarget()))
         .andThen(Commands.runEnd(
             () -> m_storage.setRollers(RollerState.FORWARD),
-            () -> this.stopFuelShooter(),
+            () -> {
+              this.stopFuelShooter();
+              m_storage.setRollers(RollerState.OFF);
+            },
             m_storage));
   }
 
-  //TODO - Stop Shooting Command
-  public void stopFuelShooter(){
-      //m_storage.setRollers(RollerState.OFF);
-      // stop FlyWheel
-      // set Hood to default posiiton
+  // TODO - Stop Shooting Command
+  public void stopFuelShooter() {
+    // m_storage.setRollers(RollerState.OFF);
+    // stop FlyWheel
+    // set Hood to default posiiton
   }
 
   // TODO: Add function for LaunchAngle
@@ -107,7 +124,7 @@ public class RobotContainer {
   }
 
   // TODO: Add function for Flywheel speed
-  public AngularVelocity getFlywheelSpeed(Distance distance) {
-    return AngularVelocity.ofBaseUnits(0.0, RotationsPerSecond);
+  public LinearVelocity getFlywheelSpeed(Distance distance) {
+    return FeetPerSecond.of(0.0);
   }
 }
