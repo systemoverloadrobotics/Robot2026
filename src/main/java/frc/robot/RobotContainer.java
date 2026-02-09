efe0a26201ebeecd243a1befbd6a282833e5a299
jeannebrigitjudy <jeanne.brigit.judy@gmail.com>
2026-02-08 16:33:51 -0800

    Shuttle Fuel
    command to shuttle fuel from one alliance zone to the other in reverse direction

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,13 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubSystem;
 import frc.robot.subsystems.Storage;
 import frc.robot.subsystems.Storage.RollerState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 
 import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.FeetPerSecond;
@@ -15,15 +19,19 @@ import static edu.wpi.first.units.Units.FeetPerSecond;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ShooterSubSystem shooter = new ShooterSubSystem();
   private final Storage storage = new Storage();
+  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+
 
   private double targetFlywheelVelocity = 50.0; // ft/s
 
@@ -51,6 +59,12 @@ public class RobotContainer {
 
       joystick.x().onTrue(Commands.runOnce(
         () -> targetFlywheelVelocity -= 1.0, (Subsystem)null));
+        
+      
+      joystick.y().whileTrue(Commands.run(
+      () -> intakeSubsystem.setPower(Constants.Intake.OuttakePower), intakeSubsystem))
+      .onFalse(Commands.runOnce(() -> intakeSubsystem.stop(), intakeSubsystem));
+
   }
 
   public Command shootFuel(Distance distance) {
@@ -72,6 +86,20 @@ public class RobotContainer {
             },
             storage));
   }
+  private void ShuttleFuel() {
+    storage.setRollers(RollerState.REVERSE);  // ← Add this line
+    intakeSubsystem.setPower(Constants.Intake.OuttakePower);
+}
+
+private void stopShuttle() {
+    storage.setRollers(RollerState.OFF);  // ← Add this to stop storage
+    intakeSubsystem.stop();
+}
+
+private void stopIntake() {
+  intakeSubsystem.stop();
+}
+
 
   // TODO - Stop Shooting Command
   public void stopFuelShooter() {
